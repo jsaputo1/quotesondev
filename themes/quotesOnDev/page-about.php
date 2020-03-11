a46816201c21bb093e40feab5450b787b8dce096
John Saputo <John@John-MBP.local>
2020-03-11 10:40:17 -0400

    style: added content to About page

---
+++ themes/quotesOnDev/page-about.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<h1>About</h1>
+
+<p>Quotes on Dev is a project site for the RED Academy Web Developer Professional program. It's used to experiement with Ajax, WP API, jQuery and other cool things</p>
+
+<p>The site is heavily inspired by Chris Coyler's <a href="#">Quotes on Design</a>.</p>
+    
+<?php get_footer();?>
