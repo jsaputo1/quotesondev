9226d7dd40fb4c2ba82681979e6de7ac1794677a
John Saputo <John@John-MBP.local>
2020-03-11 10:43:47 -0400

    style: edited links in footer menu and added margin-top

--- themes/quotesOnDev/footer.php
+++ themes/quotesOnDev/footer.php
@@ -1,8 +1,8 @@
 <?php wp_footer();?> 
 <footer>
-        <a href="#">About</a><span>|</span>
+        <a href="<?php echo get_home_url(); ?>/about">About</a><span>|</span>
         <a href="<?php echo get_home_url(); ?>/archives">Archives</a><span>|</span> 
-        <a href="#">Submit a Quote</a>
+        <a href="<?php echo get_home_url(); ?>/submit-a-quote">Submit a Quote</a>
        <p>Brought to you by<a href="#"> RED Academy</a></p>
 
 </footer>
