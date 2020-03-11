80aa1a3ed23284c6111e523102f700af3735e5fe
John Saputo <John@John-MBP.local>
2020-03-11 11:49:31 -0400

    style: added content on submit quote page and styled the form

---
+++ themes/quotesOnDev/page-submit-a-quote.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="submit-a-quote">
+  <form action="action_page.php">
+
+    <label for="author">Author</label>
+    <input type="text" id="author" name="firstname">
+
+    <label for="quote">Quote</label>
+    <input type="text" id="quote">
+
+    <label for="source-form">Where did you find this quote? (e.g. book name)</label>
+    <input type="text" id="source-form">
+
+    <label for="url-form">Provide the URL of the quote source, if available.</label>
+    <input type="text" id="url-form">
+
+
+    <input type="submit" id="submit" value="Submit Quote">
+
+  </form>
+</div> 
+
+<?php get_footer(); ?>
