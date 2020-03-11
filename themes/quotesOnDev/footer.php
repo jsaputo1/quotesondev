cf7ea7248e69aea7842527205a9336ab806346bf
John Saputo <John@John-MBP.local>
2020-03-10 21:43:50 -0400

    style: added link to Archives

--- themes/quotesOnDev/footer.php
+++ themes/quotesOnDev/footer.php
@@ -1,9 +1,11 @@
 <?php wp_footer();?> 
 <footer>
         <a href="#">About</a><span>|</span>
-        <a href="#">Archives</a><span>|</span> 
+        <a href="<?php echo get_home_url(); ?>/archives">Archives</a><span>|</span> 
         <a href="#">Submit a Quote</a>
        <p>Brought to you by<a href="#"> RED Academy</a></p>
+
 </footer>
 </body>
 </html>
+
