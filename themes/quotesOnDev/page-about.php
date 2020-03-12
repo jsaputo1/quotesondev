262fcde593aecad1e080a8b94446409edd5f4b2d
John Saputo <John@John-MBP.local>
2020-03-12 15:32:17 -0400

    style: changed font weight and styled margins on About page

--- themes/quotesOnDev/page-about.php
+++ themes/quotesOnDev/page-about.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
+<div class="about">
+    <h1>About</h1>
 
-<h1>About</h1>
+    <p>Quotes on Dev is a project site for the RED Academy Web Developer Professional program. It's used to experiement with Ajax, WP API, jQuery and other cool things</p>
 
-<p>Quotes on Dev is a project site for the RED Academy Web Developer Professional program. It's used to experiement with Ajax, WP API, jQuery and other cool things</p>
-
-<p>The site is heavily inspired by Chris Coyler's <a href="#">Quotes on Design</a>.</p>
+    <p>The site is heavily inspired by Chris Coyler's <a href="#">Quotes on Design</a>.</p>
+</div>
     
 <?php get_footer();?>
