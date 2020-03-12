a2e939d503d101ee1176acb1bab81e5bdc11fb88
John Saputo <John@John-MBP.local>
2020-03-12 11:39:39 -0400

    fix: removed incorrect code for nonce in functions page

--- themes/quotesOnDev/functions.php
+++ themes/quotesOnDev/functions.php
@@ -91,12 +91,12 @@ function red_scripts() {
    wp_localize_script( 'red_comments', 'red_vars', array(
 	   'rest_url' => esc_url_raw( rest_url() ),
 	   'wpapi_nonce' => wp_create_nonce( 'wp_rest' ),
-	   'ajax_url' => admin_url( 'admin-ajax.php' ),
-	   'comment_nonce' => wp_create_nonce( 'red_comment_status' ),
-	   'post_id' => get_the_ID(),
-	   'nonce' => wp_create_nonce( 'wp_rest' ),
-      'success' => 'Thanks, your submission was received!',
-      'failure' => 'Your submission could not be processed.'
+	//    'ajax_url' => admin_url( 'admin-ajax.php' ),
+	//    'comment_nonce' => wp_create_nonce( 'red_comment_status' ),
+	//    'post_id' => get_the_ID(),
+	//    'nonce' => wp_create_nonce( 'wp_rest' ),
+    //   'success' => 'Thanks, your submission was received!',
+    //   'failure' => 'Your submission could not be processed.'
    ) );
  }
  add_action( 'wp_enqueue_scripts', 'red_scripts' );
