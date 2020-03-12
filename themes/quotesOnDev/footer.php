7d56233887659c2fca1a0ca4bbb4aa1fd60f0dda
John Saputo <John@John-MBP.local>
2020-03-12 16:06:55 -0400

    style: added tablet responsive changes to main page and footer

--- themes/quotesOnDev/footer.php
+++ themes/quotesOnDev/footer.php
@@ -3,6 +3,8 @@
         <a href="<?php echo get_home_url(); ?>/about">About</a><span>|</span>
         <a href="<?php echo get_home_url(); ?>/archives">Archives</a><span>|</span> 
         <a href="<?php echo get_home_url(); ?>/submit-a-quote">Submit a Quote</a>
+        <span class="hidden-dash">|</span>
+       
        <p>Brought to you by<a href="#"> RED Academy</a></p>
 
 </footer>
