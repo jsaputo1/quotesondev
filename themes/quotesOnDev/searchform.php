222b4e8ddd9525b97ec78831679ca830e6ad9b3f
John Saputo <John@John-MBP.local>
2020-03-12 15:46:21 -0400

    style: styled search butt on 404 page and added FA icon to search

--- themes/quotesOnDev/searchform.php
+++ themes/quotesOnDev/searchform.php
@@ -1,9 +1,7 @@
 <form role type="search" name="s" action="<?php echo home_url('/')?>"> 
     <fieldset>
-        <a href="#" class="search-toggle">
-        <i class="fas fa-search"></i>
-        </a>
         <label class="search-field">
-            <input placeholder="Type and hit enter" type="search" name="s" value="<?php echo esc_attr(get_search_query()); ?>" class="search-input">
+            <input placeholder="SEARCH..." type="search" name="s" value="<?php echo esc_attr(get_search_query()); ?>" class="search-input">
+            <button class="search-submit"><i class="fa fa-search"></i></button>
         </fieldset>
 </form>
