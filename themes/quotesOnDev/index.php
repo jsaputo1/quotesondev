14ea6cc2f5f7e6a8c63b2d6c4f4d5e0b823e4148
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-14 18:28:07 -0400

    changed fa quotes to pseudo class

--- themes/quotesOnDev/index.php
+++ themes/quotesOnDev/index.php
@@ -1,45 +1,36 @@
 <?php get_header(); ?>
 
-
 <!-- Random quote loop -->
 <div class="random-quote">
-
-
     <?php
-        $args = array( 
-            'post_type' => 'post', 
-            'orderby' => 'rand',
-            'numberposts' => 1
-            );
-        $quotes = get_posts( $args ); 
+    $args = array( 
+    'post_type' => 'post', 
+    'orderby' => 'rand',
+    'numberposts' => 1
+    );
+    $quotes = get_posts( $args ); 
     ?>
 
     <?php foreach ( $quotes as $post ) : setup_postdata( $post ); ?>
-<div class="desktop-container">
-    <!-- Random quote content -->
-    <i class="fas fa-quote-left" id="left"></i>
+
     <div class="home-quote">
-    
-
-        <?php the_content(); ?> 
+        <!-- Random quote content -->
+        <q class = 'quote-content'>
+            <?php the_content(); ?> 
+        </q>
     </div>
-    <i class="fas fa-quote-right" id="right"></i>
 
-    </div>
-        
     <div class="quote-section">
         <span class="author">— <?php the_title(); ?></span>
 
         <div class="source">
         <a href="<?php echo get_post_meta( get_the_ID(), '_qod_quote_source_url', true );?>" class="source-link" target="new"> 
-        
+
         <span><?php echo get_post_meta( get_the_ID(), '_qod_quote_source', true );?></a></span>
         </div>
     </div>
 
-    
     <?php endforeach;?>
-   
 
 </div>
 
