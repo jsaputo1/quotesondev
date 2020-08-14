d4c2e2e8a0427b304fd4d04e4e6748f9e3e60b5c
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-14 18:33:59 -0400

    added section for home page

--- themes/quotesOnDev/index.php
+++ themes/quotesOnDev/index.php
@@ -1,41 +1,43 @@
 <?php get_header(); ?>
 
-<!-- Random quote loop -->
-<div class="random-quote">
-    <?php
-    $args = array( 
-    'post_type' => 'post', 
-    'orderby' => 'rand',
-    'numberposts' => 1
-    );
-    $quotes = get_posts( $args ); 
-    ?>
+    <section class="random-quote-home-page">
+    <!-- Random quote loop -->
+    <div class="random-quote">
+        <?php
+        $args = array( 
+        'post_type' => 'post', 
+        'orderby' => 'rand',
+        'numberposts' => 1
+        );
+        $quotes = get_posts( $args ); 
+        ?>
 
-    <?php foreach ( $quotes as $post ) : setup_postdata( $post ); ?>
+        <?php foreach ( $quotes as $post ) : setup_postdata( $post ); ?>
 
-    <div class="home-quote">
-        <!-- Random quote content -->
-        <q class = 'quote-content'>
-            <?php the_content(); ?> 
-        </q>
-    </div>
-
-    <div class="quote-section">
-        <span class="author">— <?php the_title(); ?></span>
-
-        <div class="source">
-        <a href="<?php echo get_post_meta( get_the_ID(), '_qod_quote_source_url', true );?>" class="source-link" target="new"> 
-
-        <span><?php echo get_post_meta( get_the_ID(), '_qod_quote_source', true );?></a></span>
+        <div class="home-quote">
+            <!-- Random quote content -->
+            <q class = 'quote-content'>
+                <?php the_content(); ?> 
+            </q>
         </div>
+
+        <div class="quote-section">
+            <span class="author">— <?php the_title(); ?></span>
+
+            <div class="source">
+            <a href="<?php echo get_post_meta( get_the_ID(), '_qod_quote_source_url', true );?>" class="source-link" target="new"> 
+
+            <span><?php echo get_post_meta( get_the_ID(), '_qod_quote_source', true );?></a></span>
+            </div>
+        </div>
+
+        <?php endforeach;?>
+
     </div>
 
-    <?php endforeach;?>
-
-</div>
-
-<!-- Random quote generator -->
-<button id="quote-button">Show Me Another!</button>
+    <!-- Random quote generator -->
+    <button id="quote-button">Show Me Another!</button>
+</section>
 
 
 <!-- Footer -->
