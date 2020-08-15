2c63367077b61c6c5da5aa71725c1ae80ff595c9
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-14 23:42:52 -0400

    changed input to textarea

--- themes/quotesOnDev/page-submit-a-quote.php
+++ themes/quotesOnDev/page-submit-a-quote.php
@@ -2,22 +2,15 @@
 
 <div class="submit-a-quote">
   <form>
-
     <label for="author">Author</label>
-    <input type="text" id="author-form" name="firstname">
-
+    <textarea name="author" id="author-form" name="firstname"></textarea>
     <label for="quote">Quote</label>
-    <input type="text" id="quote-form">
-
+    <textarea name="quote" id="quote-form"></textarea>
     <label for="source-form">Where did you find this quote? (e.g. book name)</label>
-    <input type="text" id="source-form">
-
+    <textarea name="source" id="source-form"></textarea>
     <label for="url-form">Provide the URL of the quote source, if available.</label>
-    <input type="text" id="url-form">
-
-
+    <textarea name="url" id="url-form"></textarea>
     <input type="submit" id="submit" value="Submit Quote" />
-
   </form>
 </div> 
 
