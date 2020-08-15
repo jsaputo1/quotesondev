2d157e37c0323556f0567107fa66feda0765f3ca
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-08-15 01:45:03 -0400

    fixed bug to prevent authors from showing up multiple times

--- themes/quotesOnDev/page-archives.php
+++ themes/quotesOnDev/page-archives.php
@@ -8,38 +8,44 @@
 
     <?php
         $args = array( 
-            'order' => 'ASC',
             'orderby' => 'title',
+            'order' => 'ASC',
+            'hide_empty' => 1,
             'numberposts' => -1
-
         );
         $quotes = get_posts( $args ); 
+        $list = array();
     ?>
-
-    
 
     <ul>
         <?php foreach ( $quotes as $post ) : setup_postdata( $post ); ?>
+        <?php 
+            if(in_array(get_the_title(), $list)){ continue; }
+            $list[] = get_the_title();
+        ?>
         <li> 
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
         </li>
         <?php endforeach;?>
     </ul>
-
+    <?php  wp_reset_postdata(); ?>
     <!-- Category loop -->
     <h2>Categories</h2>
     <ul>
     <?php 
     $args = array(
-        'order' => 'ASC', 
+        'orderby' => 'name',
+        'order' => 'ASC',
+        'hide_empty' => 1, 
+        'number' => 9999 
     ); 
     $cats = get_categories($args);
     foreach ($cats as $cat) {           
         $cat_id= $cat->term_id;
         $cat_name= $cat->name; ?>
         <li>
-
-        <?php echo '<a href="' . get_category_link( $cat_id ) . '">'.$cat->name.'</a>'; ?>    </li>
+        <?php echo '<a href="' . get_category_link( $cat_id ) . '">'.$cat->name.'</a>'; ?>   
+        </li>
 
     <?php  } ?>
     </ul>
@@ -60,7 +66,6 @@
             </li>     
     <?php  } ?>
     </ul>
-
 </div>
 
 <!-- Footer -->
